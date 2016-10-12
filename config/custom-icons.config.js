3e73fee07a0c56570e580cd5c756c7e6785f1488
Aldrin Tingson <aldrin@Aldrins-MacBook-Pro.local>
2016-10-13 07:33:01 +0800

    removed previous icon files. added icons from the author websites

--- config/custom-icons.config.js
+++ config/custom-icons.config.js
@@ -1,10 +1,25 @@
 // customIcons config
 module.exports = {
    iconSets: [
-      {
-         src: 'heyblue_icons/heyblue/*.svg',
-         name: 'HeyBlue',
-         id: 'heyblue'
-      }
+     {
+       src: 'heyblue_icons/evil-icons/*.svg',
+       name: 'EvilIcons',
+       id: 'evilIcons'
+     },
+     {
+       src: 'heyblue_icons/linecons/*.svg',
+       name: 'Linecons',
+       id: 'linecons'
+     },
+     {
+       src: 'heyblue_icons/typicons/*.svg',
+       name: 'Typicons',
+       id: 'typicons'
+     },
+     {
+       src: 'heyblue_icons/font-awesome/*.svg',
+       name: 'FontAwesome',
+       id: 'fontAwesome'
+     }
    ]
 };
